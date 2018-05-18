8373e69115d8cc8f407b72270a3888b00c15f327
Markus Ressel <markus.ressel@berlin.de>
2018-05-19 01:32:10 +0200

    merged classes

--- library/src/main/java/de/markusressel/kutepreferences/library/persistence/DefaultKutePreferenceDataProvider.kt
+++ library/src/main/java/de/markusressel/kutepreferences/library/persistence/DefaultKutePreferenceDataProvider.kt
@@ -3,23 +3,42 @@ package de.markusressel.kutepreferences.library.persistence
 import android.content.Context
 import de.markusressel.kutepreferences.library.preference.KutePreferenceItem
 import de.markusressel.typedpreferences.PreferenceItem
+import de.markusressel.typedpreferences.PreferencesHandlerBase
 
-class DefaultKutePreferenceDataProvider(context: Context) : KutePreferenceDataProvider {
+/**
+ * Default implementation of a KutePreferenceDataProvider using a SharedPreferences backed store
+ */
+class DefaultKutePreferenceDataProvider(context: Context) : PreferencesHandlerBase(context),
+        KutePreferenceDataProvider {
 
-    private val preferenceHandler = PreferenceHandler(context)
+    // be sure to override the get() method
+    override var sharedPreferencesName: String? = null
+        get() = "kute_preferences"
+
+    private val currentPreferenceItems: MutableMap<String, PreferenceItem<*>> = mutableMapOf()
+
+    override val allPreferenceItems: Set<PreferenceItem<*>>
+        get() {
+            return currentPreferenceItems.values.toSet()
+        }
+
+    fun <PreferenceType : Any> getPreferenceItem(
+            kutePreference: KutePreferenceItem<PreferenceType>): PreferenceItem<PreferenceType> {
+        val key = context.getString(kutePreference.key)
+
+        return currentPreferenceItems.getOrPut(key) {
+            PreferenceItem(kutePreference.key, kutePreference.getDefaultValue())
+        } as PreferenceItem<PreferenceType>
+    }
 
     override fun <DataType : Any> storeValue(kutePreference: KutePreferenceItem<DataType>, newValue: DataType) {
-        val preferenceItem: PreferenceItem<DataType> = preferenceHandler
-                .getPreferenceItem(kutePreference)
-        preferenceHandler
-                .setValue(preferenceItem, newValue)
+        val preferenceItem: PreferenceItem<DataType> = getPreferenceItem(kutePreference)
+        setValue(preferenceItem, newValue)
     }
 
     override fun <DataType : Any> getValue(kutePreference: KutePreferenceItem<DataType>): DataType {
-        val preferenceItem: PreferenceItem<DataType> = preferenceHandler
-                .getPreferenceItem(kutePreference)
-        return preferenceHandler
-                .getValue(preferenceItem)
+        val preferenceItem: PreferenceItem<DataType> = getPreferenceItem(kutePreference)
+        return getValue(preferenceItem)
     }
 
 }
